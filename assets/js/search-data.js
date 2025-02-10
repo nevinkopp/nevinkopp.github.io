a28dbe6afa08bdb93f9600fe492e3db99aee6003
nevinkopp <nevinkopp@gmail.com>
2025-02-10 23:39:26 +0000

    Deploying to gh-pages from @ nevinkopp/nevinkopp.github.io@9bafd3f75ed9005ea2004ef204f0410b893bea34 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-resume",
+          title: "resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
