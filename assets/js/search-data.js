f6d44a297700a533ad537f4b1b7c3ec4e4231c70
nevinkopp <nevinkopp@gmail.com>
2025-02-10 06:26:08 +0000

    Deploying to gh-pages from @ nevinkopp/nevinkopp.github.io@5939e8f3509e64ac35ed9ee17c92d6e5786b3ec9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,7 +499,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/Nevin Kopp", "_blank");
+          window.open("https://www.linkedin.com/in/nevin-kopp", "_blank");
         },
       },{
       id: 'light-theme',
