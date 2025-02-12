6d7f1b158c7b8f84cee9a324facf0677d6cb93a7
nevinkopp <nevinkopp@gmail.com>
2025-02-12 17:39:32 +0000

    Deploying to gh-pages from @ nevinkopp/nevinkopp.github.io@245b83c3dc643e840eac67361ccf1e5fb7aac99a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -401,26 +401,16 @@ ninja.data = [{
           description: "Automated bread maker",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
