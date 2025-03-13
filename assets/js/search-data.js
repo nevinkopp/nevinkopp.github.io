b52f33849cb0fc3dbac05a253fb6b555f0089ec4
nevinkopp <nevinkopp@gmail.com>
2025-03-13 21:45:25 +0000

    Deploying to gh-pages from @ nevinkopp/nevinkopp.github.io@62f06d6b748bed4848b543b13c7e605a09d396e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,8 +406,8 @@ ninja.data = [{
           description: "Seek and mildly annoy",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-path-planning",
-          title: 'Path Planning',
+            },},{id: "projects-autonomous-navigation",
+          title: 'Autonomous Navigation',
           description: "A* path planning with PID control and polynomial time scaling",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
